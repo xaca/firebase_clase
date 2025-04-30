1d1d9ac86f46807e4e0c5b6f47d7cbb38a9d3fe0
Andres Bedoya <and1res@hotmail.com>
2025-04-30 18:13:38 -0500

    fix:security issue config

--- src/libs/utils/config.ts
+++ src/libs/utils/config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyARDzkdwaNQ2v9RyaSLH2qdhyBDZt587p0",
-    authDomain: "fullstack-fed2d.firebaseapp.com",
-    databaseURL: "https://fullstack-fed2d-default-rtdb.firebaseio.com",
-    projectId: "fullstack-fed2d",
-    storageBucket: "fullstack-fed2d.appspot.com",
-    messagingSenderId: "81689960691",
-    appId: "1:81689960691:web:408444ef49fe0feb709786"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
   
